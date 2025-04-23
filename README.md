2dc139dff274615422a95efa021398c03a43361b
wryk <wryk@wryk.moe>
2025-04-23 19:33:40 +0200

    Update favicon and guerilla pic (thx Tixie), remove iconify-icon

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo
+
+- Generate icons with parcel macros, @iconify/json and @iconify/utils
+- Use parcel glob resolver
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "iconify-icon"
-
-
-// Looks like target "_blank" in web context is broken on firefox because of https://bugzilla.mozilla.org/show_bug.cgi?id=646552 :/
